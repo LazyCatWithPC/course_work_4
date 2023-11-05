875b131931fa431673dc4bb69b383801dc84800e
LazyCatWithPC <i.treukhov@gmail.com>
2023-11-05 14:44:45 +0400

    updated website_work.py

--- src/website_work.py
+++ src/website_work.py
@@ -10,6 +10,7 @@ class Website(ABC):
 
 
 class HeadHunter(Website):
+
     def get_vacancies_data(self, page: int, keyword: str):
         params = {
             "text": keyword,
@@ -32,7 +33,6 @@ class HeadHunter(Website):
 
 
 class SuperJob(Website):
-
     API = "v3.r.137937370.924277268373661c326e2d86d1509666810c1dfc.f5bfa2098a0696b48d0d8d646e960dd6c4973aab"
 
     def __init__(self):
@@ -63,3 +63,81 @@ class SuperJob(Website):
             js_obj = json.loads(self.get_vacancies_data(page, keyword))
             js_list.extend(js_obj["objects"])
         return js_list
+
+
+class Vacancies:
+    def __init__(self, name=None, salary=None, salary_to=None, salary_currency=None,
+                 town=None, experience=None, url=None):
+        self.name = name
+        self.salary = salary
+        self.salary_to = salary_to
+        self.salary_currency = salary_currency
+        self.town = town
+        self.experience = experience
+        self.url = url
+
+    def __repr__(self):
+        return (f"Vacancy(name={self.name}, salary={self.salary}, salary_to={self.salary_to}, "
+                f"salary_currency={self.salary_currency}, town={self.town},"
+                f"experience={self.experience}, url={self.url}")
+
+    def __str__(self):
+        return (f"Вакансия: {self.name}. Зарплата {self.salary}-{self.salary_to} {self.salary_currency}. "
+                f"Город: {self.town}"
+                f"Ссылка: {self.url}"
+                f"Опыт работы:{self.experience}")
+
+    def __le__(self, other):
+        return int(self.salary) <= int(other.salary)
+
+    def __lt__(self, other):
+        return int(self.salary) < int(other.salary)
+
+    def __ge__(self, other):
+        return int(self.salary) >= int(other.salary)
+
+    def __gt__(self, other):
+        return int(self.salary) > int(other.salary)
+
+    def __eq__(self, other):
+        return int(self.salary) == int(other.salary)
+
+
+class SuperJobVacancies(Vacancies, SuperJob):
+    def load_vacancies(self, keyword: str):
+        list_vacancies = []
+        for vacancy in SuperJob.vacancies_to_json(self, keyword):
+            new_vacancy = SuperJobVacancies(name=vacancy["profession"],
+                                            salary=vacancy["payment_from"],
+                                            salary_to=vacancy["payment_to"],
+                                            salary_currency=vacancy["currency"],
+                                            town=vacancy["town"]["title"],
+                                            experience=vacancy["experience"]["title"],
+                                            url=vacancy["link"])
+            list_vacancies.append(new_vacancy)
+
+        return list_vacancies
+
+
+class HeadHunterVacancies(Vacancies, HeadHunter):
+    def load_vacancies(self, keyword: str):
+        list_of_vacancies = []
+        for vacancy in HeadHunter.vacancies_to_json(self, keyword):
+            try:
+                new_vacancy = HeadHunterVacancies(name=vacancy["name"],
+                                                  salary=vacancy["salary"]["from"],
+                                                  salary_to=vacancy["salary"]["to"],
+                                                  salary_currency=vacancy["salary"]["currency"],
+                                                  town=vacancy["area"]["name"],
+                                                  experience=vacancy["experience"]["name"],
+                                                  url=vacancy["alternate_url"])
+                list_of_vacancies.append(new_vacancy)
+            except TypeError:
+                new_vacancy = HeadHunterVacancies(name=vacancy["name"],
+                                                  salary=None,
+                                                  town=vacancy["area"]["name"],
+                                                  experience=vacancy["experience"]["name"],
+                                                  url=vacancy["alternate_url"])
+                list_of_vacancies.append(new_vacancy)
+
+        return list_of_vacancies
